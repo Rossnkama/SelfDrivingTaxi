27bb347fde0d196c8a4da33f136aa8078fb16924
Ross Nkama <ross.nkama@adacollege.org.uk>
2017-11-01 23:26:44 +0000

    Initial commit of game template for self driving car

---
+++ ai.py
@@ -0,0 +1,15 @@
+# AI for Self Driving Car
+
+# Importing the libraries
+import numpy as np
+import random
+import os
+import torch
+import torch.nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
+
+# Creating the architecture of the neural network
+
